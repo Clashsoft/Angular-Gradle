e37f6f9353c0c28dc74b957a27d916ffefeb3025
Adrian Kunz <clashsoft@hotmail.com>
2020-02-09 19:22:59 +0100

    Add AngularGradlePlugin.groovy

---
+++ src/main/groovy/de/clashsoft/gradle/angular/AngularGradlePlugin.groovy
@@ -0,0 +1,10 @@
+package de.clashsoft.gradle.angular
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AngularGradlePlugin implements Plugin<Project> {
+	@Override
+	void apply(Project project) {
+	}
+}
